31ed88211125cf3072b3d330d9d841fa02758854
Jacksson Andrey <jaab.andrey@gmail.com>
2020-11-30 11:54:58 -0300

    images of the technologies

--- src/App.tsx
+++ src/App.tsx
@@ -12,23 +12,48 @@ import ImgProject from './assets/images/nlw2.png';
 import ImgProject2 from './assets/images/nlw2.2.png';
 import ImgProject3 from './assets/images/nlw3.png';
 import ImgPerfil from './assets/images/perfil.jpg';
+import LogoPython from './assets/images/logo-python.png';
+import LogoJs from './assets/images/logo-js.png';
+import LogoNode from './assets/images/logo-nodejs.png';
+import LogoTs from './assets/images/logo-ts.png';
+import LogoPhp from './assets/images/logo-php.png';
+import LogoDjango from './assets/images/logo-django.png';
+import LogoBootstrap from './assets/images/logo-bootstrap.png';
+import LogoJquery from './assets/images/logo-jquery.png';
+import LogoMaterial from './assets/images/logo-materialui.png';
+import LogoLaravel from './assets/images/logo-laravel.png';
+import LogoReact from './assets/images/logo-react.png';
+import LogoGithub from './assets/images/logo-github.png';
+import LogoCss from './assets/images/logo-css.png';
+import LogoHtml from './assets/images/logo-html.png';
+import LogoMysql from './assets/images/logo-mysql.png';
+import LogoAws from './assets/images/logo-aws.png';
+import LogoHeroku from './assets/images/logo-heroku.png';
 
 import './App.css';
 
 const App: React.FC = () => {
   return (
     <main className="page-main">
-      <header className="page-header">
+      <header id="page-header" className="page-header">
         <nav className="menu-header">
           <span className="img-logo-menu">
             <img src={LogoMenu} alt="Andrey Araújo" />
           </span>
 
           <ul className="list-menu">
-            <li>Home</li>
-            <li>Sobre</li>
-            <li>Projetos</li>
-            <li>Contatos</li>
+            <li>
+              <a href="#page-header">Home</a>
+            </li>
+            <li>
+              <a href="#about-me">Sobre</a>
+            </li>
+            <li>
+              <a href="#my-projects">Projetos</a>
+            </li>
+            <li>
+              <a href="#footer">Contatos</a>
+            </li>
           </ul>
         </nav>
       </header>
@@ -63,7 +88,7 @@ const App: React.FC = () => {
         </aside>
       </div>
 
-      <div className="my-projects">
+      <div id="my-projects" className="my-projects">
         <h1>Meus Projetos</h1>
         <h4>
           Abaixo estão alguns dos projetos pessoais e profissionais que já
@@ -157,7 +182,7 @@ const App: React.FC = () => {
         </article>
       </div>
 
-      <div className="about-me">
+      <div id="about-me" className="about-me">
         <h1>Sobre</h1>
         <div className="area-about-me">
           <aside className="text-about-me">
@@ -185,7 +210,99 @@ const App: React.FC = () => {
             <img src={ImgPerfil} alt="Andrey Araújo" />
           </aside>
         </div>
+
+        <div className="area-btn">
+          <button type="button" className="btn-view-curriculum">
+            Veja meu currículo
+          </button>
+        </div>
       </div>
+
+      <div id="technologies" className="technologies">
+        <h1>Tecnologias</h1>
+        <div className="images-techs">
+          <ul className="list-techs-1">
+            <li>
+              <img src={LogoPython} alt="Python" />
+            </li>
+            <li>
+              <img src={LogoJs} alt="Javascript" />
+            </li>
+            <li>
+              <img src={LogoNode} alt="Nodejs" />
+            </li>
+            <li>
+              <img src={LogoTs} alt="Typescript" />
+            </li>
+            <li>
+              <img src={LogoPhp} alt="PHP" />
+            </li>
+          </ul>
+
+          <ul className="list-techs-2">
+            <li>
+              <img src={LogoDjango} alt="Django" />
+            </li>
+            <li>
+              <img src={LogoBootstrap} alt="Bootstrap" />
+            </li>
+            <li>
+              <img src={LogoJquery} alt="JQuery" />
+            </li>
+            <li>
+              <img src={LogoMaterial} alt="Material-ui" />
+            </li>
+            <li>
+              <img src={LogoLaravel} alt="Laravel" />
+            </li>
+            <li>
+              <img src={LogoReact} alt="React" />
+            </li>
+          </ul>
+
+          <ul className="list-techs-3">
+            <li>
+              <img src={LogoGithub} alt="Github" />
+            </li>
+            <li>
+              <img src={LogoCss} alt="CSS" />
+            </li>
+            <li>
+              <img src={LogoHtml} alt="HTML" />
+            </li>
+            <li>
+              <img src={LogoMysql} alt="MySQL" />
+            </li>
+            <li>
+              <img src={LogoAws} alt="AWS" />
+            </li>
+            <li>
+              <img src={LogoHeroku} alt="Heroku" />
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <footer id="footer" className="footer">
+        <ul className="list-social">
+          <li>
+            <FaInstagramSquare size={24} />
+          </li>
+          <li>
+            <FaLinkedin size={24} />
+          </li>
+          <li>
+            <FaFacebookSquare size={24} />
+          </li>
+          <li>
+            <FaGithubSquare size={24} />
+          </li>
+        </ul>
+        <img src={Logo} alt="Andrey Araújo" className="img-footer" />
+
+        <p>Desenvolvido e criado por Andrey Araújo - 2020</p>
+        <p>andreyaraujo.dev</p>
+      </footer>
     </main>
   );
 };
